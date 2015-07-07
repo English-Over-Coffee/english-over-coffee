22da5ea5bcbdb72568d70822092f0b263d3469af
Sheldon J. Johnson <shayolden@hotmail.com>
2015-07-08 07:40:38 +0900

    allow signed in teacher to edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+        :first_name, :email, :password, :current_password, :photo, :home_town, 
+        :education, :hobbies, :motto, :advice, :strengths, :arrived_in_country,
+        :other_languages_spoken, :peronal_message) }
+    end
 end
