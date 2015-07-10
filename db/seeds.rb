fdd8ba640fb259466822d1f4522db29820c40c18
zel <shayolden@hotmail.com>
2015-07-10 13:25:00 +0900

    Update seeds.rb
    Needs to be tested as working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@teacher = Teacher.create {email: "elvis@presley.com", password: "password", password_confirmation: "password", first_name: "Elvis", last_name: "Presley" }
+
+@teacher2 = Teacher.create {email: "mahatma@gandhi.com", password: "password", password_confirmation: "password", first_name: "Mahatma", last_name: "Gandhi" }
+
+@teacher3 = Teacher.create {email: "mahatma@gandhi.com", password: "password", password_confirmation: "password", first_name: "Mahatma", last_name: "Gandhi" }
+
+@teacher2 = Teacher.create {email: "steve@urkel.com", password: "password", password_confirmation: "password", first_name: "Steve", last_name: "Urkel" }
