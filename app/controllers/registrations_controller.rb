22225c8ad64fa38bd8ea8cef715506700180aa32
Sheldon J. Johnson <shayolden@hotmail.com>
2015-07-24 03:26:33 +0900

    redirect to edit teacher page after signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,8 @@ class RegistrationsController < Devise::RegistrationsController
     def after_update_path_for(resource)
       teacher_path(resource)
     end
+
+    def after_sign_up_path_for(resource)
+      edit_teacher_registration_path # Or :prefix_to_your_route
+    end
 end
