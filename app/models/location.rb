e030031d59a3107019e6c912d16b6fc8bbaf8ff1
Sheldon J. Johnson <shayolden@hotmail.com>
2015-07-29 01:34:45 +0900

    change teacher dashboard locations layout to use media object styling

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,9 @@ class Location < ActiveRecord::Base
   belongs_to :teacher, dependent: :destroy
 
   has_attached_file :avatar,
-                    :styles => { :square => "512x512#", :medium => "300x300>", :thumb => "100x100#" },
+                    :styles => { :square => "512x512#",
+                                 :medium => "300x300>",
+                                 :thumb => "100x100#" },
                     :default_url => "/images/:style/no_image.png"
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
