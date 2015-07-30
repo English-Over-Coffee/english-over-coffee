913c94d811c85c2828ccfcf27ee7e3182bd226df
Sheldon J. Johnson <shayolden@hotmail.com>
2015-07-30 15:12:54 +0900

    add addresses to database

--- db/migrate/20150727153949_create_locations.rb
+++ db/migrate/20150727153949_create_locations.rb
@@ -3,6 +3,11 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.string :name
       t.string :homepage
+      t.string :street_address
+      t.string :ward
+      t.string :prefecture
+      t.string :city
+      t.string :post_code
       t.references :teacher, index: true, foreign_key: true
 
       t.timestamps null: false
